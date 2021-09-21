e56825acad412d653464cde05e114047bab7cb74
Moazzam-sayyed <moazzamsayyed09@gmail.com>
2021-09-21 11:12:12 +0530

    Initial Commit

---
+++ HR Management System/src/com/model/Role.java
@@ -0,0 +1,25 @@
+package com.model;
+
+public class Role {
+	
+	private int rolid;
+	private String roleNmae;
+	public Role(int rolid, String roleNmae) {
+		super();
+		this.rolid = rolid;
+		this.roleNmae = roleNmae;
+	}
+	public int getRolid() {
+		return rolid;
+	}
+	public void setRolid(int rolid) {
+		this.rolid = rolid;
+	}
+	public String getRoleNmae() {
+		return roleNmae;
+	}
+	public void setRoleNmae(String roleNmae) {
+		this.roleNmae = roleNmae;
+	}
+
+}
